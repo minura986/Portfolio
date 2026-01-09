9ad34f85de6399881b6d246ee70c9e088a63d148
Minura <mjleoofficial@gmail.com>
2026-01-09 21:42:36 +0530

    number

--- my-portfolio/src/components/contact/Contact.jsx
+++ my-portfolio/src/components/contact/Contact.jsx
@@ -34,7 +34,7 @@ const Contact = () => {
                         <i className="bx bx-phone contact__card-icon"></i>
 
                         <h3 className="contact__card-title">Call Me</h3>
-                        <span className="contact__card-data">072 363 1923</span>
+                        <span className="contact__card-data">077 063 1923</span>
 
                         <a href="tel:0723631923" className="contact__button">Call Now <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
                     </div>
